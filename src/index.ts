2529ccc02498eb64ce79062677186bdb3184fbb9
Oleksii <kespa71for@gmail.com>
2025-11-03 11:06:24 +0100

    chore(app): init nunjucks & prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
+import "dotenv/config";
+
+import nunjucks from "nunjucks";
 import express from "express";
+import cors from "cors";
+
+import type { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.use(cors());
+
+nunjucks.configure("src/view", {
+    autoescape: true,
+    express: app,
+    watch: true
+})
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("index.html", {
+    title: "Main page",
+    test: "Test "
+  })
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
